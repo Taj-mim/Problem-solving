7249b07a77e0b44912cdf88959a9b086f604d12e
Taj-mim <tajmim915@gmail.com>
2026-06-24 02:20:08 +0600

    add some solving details

--- HackerRank-with Python/Easy/Find a string.py
+++ HackerRank-with Python/Easy/Find a string.py
@@ -1,5 +1,8 @@
 def count_substring(string, sub_string):
     total=0
+ #s=abcdccdc,sub_string=cdc,len(s)=8,len(sub_string)=3,
+ #  for checking last two characters of string it cann't check the last 1 character 
+ # thats why we don't need to check the last two characters of string
     for i in range((len(string)-len(sub_string))+1):
         if(string[i:i+len(sub_string)])==sub_string:
             total+=1
